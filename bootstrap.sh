9d5ee111bae3fa3b1c4cb6f5896e70b1201dfe82
chris <chris.improve@outlook.com>
2017-03-09 19:59:41 +0800

    Modify bootstrap.sh for backup original vim configration

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,39 +7,11 @@
 app_name='vim_setting'
 [ -z "$APP_PATH" ] && APP_PATH="$HOME/.vim/vim_setting"
 [ -z "$REPO_URI" ] && REPO_URI="https://github.com/chrisImprove/vim.git"
+[ -z "$REPO_BRANCH" ] && REPO_BRANCH='master'
 [ -z "$VUNDLE_URI" ] && VUNDLE_URI="https://github.com/gmarik/vundle.git"
+debug_mode='0'
 
 ############################  BASIC SETUP TOOLS
-variable_set()
-{
-    if [ -z "$1" ]; then
-        error "You must have your HOME environmental variable set to continue."
-    fi
-}
-
-program_exists()
-{
-    local ret='0'
-    command -v $1 >/dev/null 2>&1 || { local ret='1'; }
-
-    # fail on non-zero return value
-    if [ "$ret" -ne 0 ]; then
-        return 1
-    fi
-
-    return 0
-}
-
-program_must_exist()
-{
-    program_exists $1
-
-    # throw error on non-zero return value
-    if [ "$?" -ne 0 ]; then
-        error "You must have '$1' installed to continue."
-    fi
-}
-
 msg() {
     printf '%b\n' "$1" >&2
 }
@@ -55,16 +27,63 @@ error() {
     exit 1
 }
 
+debug() {
+    if [ "$debug_mode" -eq '1' ] && [ "$ret" -gt '1' ]; then
+        msg "An error occurred in function \"${FUNCNAME[$i+1]}\" on line ${BASH_LINENO[$i+1]}, we're sorry for that."
+    fi
+}
+
+program_exists() {
+    local ret='0'
+    command -v $1 >/dev/null 2>&1 || { local ret='1'; }
+
+    # fail on non-zero return value
+    if [ "$ret" -ne 0 ]; then
+        return 1
+    fi
+
+    return 0
+}
+
+program_must_exist() {
+    program_exists $1
+
+    # throw error on non-zero return value
+    if [ "$?" -ne 0 ]; then
+        error "You must have '$1' installed to continue."
+    fi
+}
+
+variable_set() {
+    if [ -z "$1" ]; then
+        error "You must have your HOME environmental variable set to continue."
+    fi
+}
 
 lnif() {
     if [ -e "$1" ]; then
         ln -sf "$1" "$2"
     fi
     ret="$?"
+    debug
 }
 
 ############################ SETUP FUNCTIONS
-sync_GitRepo() {
+
+do_backup() {
+    if [ -e "$1" ] || [ -e "$2" ] || [ -e "$3" ]; then
+        msg "Attempting to back up your original vim configuration."
+        today=`date +%Y%m%d_%T`
+        for i in "$1" "$2" "$3"; do
+            [ -e "$i" ] && [ ! -L "$i" ] && mv -v "$i" "$i.$today";
+        done
+        ret="$?"
+        success "Your original vim configuration has been backed up."
+        debug
+   fi
+}
+
+sync_repo() {
     local repo_path="$1"
     local repo_uri="$2"
     local repo_branch="$3"
@@ -82,6 +101,24 @@ sync_GitRepo() {
         ret="$?"
         success "Successfully updated $repo_name"
     fi
+
+    debug
+}
+
+create_symlinks() {
+    local source_path="$1"
+    local target_path="$2"
+
+    lnif "$source_path/.vimrc"         "$target_path/.vimrc"
+
+    if program_exists "nvim"; then
+        lnif "$source_path/.vim"       "$target_path/.config/nvim"
+        lnif "$source_path/.vimrc"     "$target_path/.config/nvim/init.vim"
+    fi
+
+    ret="$?"
+    success "Setting up vim symlinks."
+    debug
 }
 
 sync_repo() {
@@ -118,6 +155,7 @@ setup_vundle() {
     export SHELL="$system_shell"
 
     success "Now updating/installing plugins using Vundle"
+    debug
 }
 
 ############################ MAIN()
@@ -125,10 +163,17 @@ variable_set "$HOME"
 program_must_exist "vim"
 program_must_exist "git"
 
-sync_GitRepo	"$APP_PATH" \
+do_backup       "$HOME/.vim" \
+                "$HOME/.vimrc" \
+                "$HOME/.gvimrc"
+
+sync_repo       "$APP_PATH" \
                 "$REPO_URI" \
-                "master" \
+                "$REPO_BRANCH" \
                 "chris-vim"
+
+create_symlinks "$APP_PATH" \
+                "$HOME"
 
 sync_repo       "$HOME/.vim/bundle/vundle" \
                 "$VUNDLE_URI" \
@@ -136,8 +181,6 @@ sync_repo       "$HOME/.vim/bundle/vundle" \
                 "vundle"
 
 setup_vundle    "$APP_PATH/.vimrc.bundles"
-
-cp $APP_PATH/.vimrc $HOME
 
 msg             "\nThanks for installing $app_name."
 msg             "© `date +%Y` http://vim.spf13.com/"
